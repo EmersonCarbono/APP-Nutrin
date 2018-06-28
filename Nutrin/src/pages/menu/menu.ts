b722b56fe0fec1910a12eabb02eae8f6d20d50f2
Emerson Carbonaro <emersonscarbonaro@gmail.com>
2018-06-28 15:11:49 -0300

    Side menu

--- Nutrin/src/pages/menu/menu.ts
+++ Nutrin/src/pages/menu/menu.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ConsultaPage } from '../consulta/consulta';
 
+
+import { UserDataProvider } from '../../providers/UserData/userData';
+
 /**
  * Generated class for the MenuPage page.
  *
@@ -13,14 +16,25 @@ import { ConsultaPage } from '../consulta/consulta';
 @Component({
   selector: 'page-menu',
   templateUrl: 'menu.html',
+  providers:[
+    UserDataProvider,
+  ]
 })
 export class MenuPage {
   rootPage = ConsultaPage;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
 
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private userDataProvider: UserDataProvider,
+  ) {}
+
+  user_data = JSON.parse(this.userDataProvider.getUserData());
+  
+  nome = this.user_data.nome;
+  email = this.user_data.email;
   ionViewDidLoad() {
-    console.log('ionViewDidLoad MenuPage');
+    console.log(this.nome);
   }
 
 }
