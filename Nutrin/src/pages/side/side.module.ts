68063a6563d533602054df629b98d99a44f28a9e
Victor Codonho <victorenrico50@gmail.com>
2018-10-19 12:44:57 -0300

    erro modalpage resolvido

--- Nutrin/src/pages/side/side.module.ts
+++ Nutrin/src/pages/side/side.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
+
 import { SidePage } from './side';
 
 @NgModule({
   declarations: [
-    SidePage,
+    SidePage
   ],
   imports: [
-    IonicPageModule.forChild(SidePage),
+    IonicPageModule.forChild(SidePage)
   ],
 })
 export class SidePageModule {}
