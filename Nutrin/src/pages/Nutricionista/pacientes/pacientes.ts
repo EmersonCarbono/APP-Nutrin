948793273de1a6b61ddf110793fe9142b82705e4
Emerson Carbonaro <emersonscarbonaro@gmail.com>
2018-10-23 09:33:23 -0300

    lista de pacientes

--- Nutrin/src/pages/Nutricionista/pacientes/pacientes.ts
+++ Nutrin/src/pages/Nutricionista/pacientes/pacientes.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { PacienteProvider } from '../../../providers/pacientes/paciente';
 
-/**
- * Generated class for the PacientesPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,13 +11,42 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PacientesPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  searchQuery: string = '';
+  lista_pacientes = new Array<any>();
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public pacienteProvaider: PacienteProvider
+  ) {
   }
 
-  
+  private iniciaizarPacientes() {
+    this.pacienteProvaider.listar_pacientes().subscribe(
+      data => {
+        const response = (data as any);
+        this.lista_pacientes = response.Dados;
+        console.log(this.lista_pacientes);
+
+      }
+    )
+  }
+
+  public buscarPaciente(ev: any) {
+
+    const val = ev.target.value;
+/*
+   if (val && val.trim() != '') {
+      this.lista_pacientes = this.lista_pacientes.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      });
+    }
+*/
+  }
 
   ionViewDidLoad() {
-    
+    this.iniciaizarPacientes();
+
   }
 
 }
