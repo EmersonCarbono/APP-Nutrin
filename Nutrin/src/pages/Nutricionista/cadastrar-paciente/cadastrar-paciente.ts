688863d25123cb537c27754d72defe2501b96b1f
Victor Codonho <victorenrico50@gmail.com>
2018-10-24 12:43:00 -0300

    validar form cadastrar paciente feito

--- Nutrin/src/pages/Nutricionista/cadastrar-paciente/cadastrar-paciente.ts
+++ Nutrin/src/pages/Nutricionista/cadastrar-paciente/cadastrar-paciente.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the CadastrarPacientePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -15,7 +9,44 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CadastrarPacientePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  cadastro:any = {};
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public formBuilder: FormBuilder
+  ) {
+
+    this.cadastro = this.formBuilder.group({
+      nome: new FormControl('', Validators.required),
+      username: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(25)
+      ])),
+      dataNascimento: new FormControl('', Validators.required),
+      sexo: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.email
+      ])),
+      celular: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(11),
+        Validators.maxLength(11)
+      ])),
+      cidade: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(30)
+      ])),
+      profissao: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(100)
+      ])),
+      altura: new FormControl('', Validators.required)
+    })
   }
 
   ionViewDidLoad() {
