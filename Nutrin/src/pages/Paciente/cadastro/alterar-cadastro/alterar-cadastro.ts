cdbc0fa11484ce3704e6b7b9aa5e9cbb419045a2
Emerson Carbonaro <emersonscarbonaro@gmail.com>
2018-10-31 10:59:41 -0300

    validações de formulario

--- Nutrin/src/pages/Paciente/cadastro/alterar-cadastro/alterar-cadastro.ts
+++ Nutrin/src/pages/Paciente/cadastro/alterar-cadastro/alterar-cadastro.ts
@@ -21,8 +21,7 @@ import { HomePacientePage } from '../../home-paciente/home-paciente';
 })
 export class AlterarCadastroPage {
 
-  dados_alterar_cadastro:any = {};
-  dados_cadastro_user = this.userDataProvider.getUserData().dados_user;
+  dados_paciente = this.userDataProvider.getUserData().dados_user;
 
 
   constructor(
@@ -32,42 +31,7 @@ export class AlterarCadastroPage {
     private pacienteProvider: PacienteProvider,
     private userDataProvider: UserDataProvider,
     public alertCtrl: AlertController
-  ) {
-
-    this.dados_alterar_cadastro = this.formBuilder.group({
-      nome: new FormControl(this.dados_cadastro_user.nome, Validators.required),
-      username: new FormControl(this.dados_cadastro_user.username, Validators.compose([
-        Validators.required,
-        Validators.minLength(5),
-        Validators.maxLength(25)
-      ])),
-      dataNascimento: new FormControl(this.dados_cadastro_user.dataNascimento, Validators.required),
-      sexo: new FormControl(this.dados_cadastro_user.sexo, Validators.required),
-      email: new FormControl(this.dados_cadastro_user.email, Validators.compose([
-        Validators.required,
-        Validators.email
-      ])),
-      celular: new FormControl(this.dados_cadastro_user.celular, Validators.compose([
-        Validators.required,
-        Validators.minLength(11),
-        Validators.maxLength(11),
-        
-      ])),
-      cidade: new FormControl(this.dados_cadastro_user.cidade, Validators.compose([
-        Validators.required,
-        Validators.minLength(4),
-        Validators.maxLength(30)
-      ])),
-      profissao: new FormControl(this.dados_cadastro_user.profissao, Validators.compose([
-        Validators.required,
-        Validators.minLength(5),
-        Validators.maxLength(100)
-      ])),
-      altura: new FormControl(this.dados_cadastro_user.altura, Validators.compose([
-        Validators.required
-      ])),
-    });
-  }
+  ) { }
 
   private showAlert(titulo, mensagem) {
     const alert = this.alertCtrl.create({
@@ -78,33 +42,12 @@ export class AlterarCadastroPage {
     alert.present();
   }
 
-  private alterarCadastro(){
-    var username_atual =  this.dados_cadastro_user.username;
-    var tipo =  this.dados_cadastro_user.tipo;
-    var objetivo =  this.dados_cadastro_user.objetivo;
 
-    var nome =  this.dados_alterar_cadastro.value.nome;
-    var username =   this.dados_alterar_cadastro.value.username;
-    var dataNascimento =   this.dados_alterar_cadastro.value.dataNascimento;
-    var sexo =   this.dados_alterar_cadastro.value.sexo;
-    var email =   this.dados_alterar_cadastro.value.email;
-    var celular =   this.dados_alterar_cadastro.value.celular;
-    var cidade =   this.dados_alterar_cadastro.value.cidade;
-    var profissao =   this.dados_alterar_cadastro.value.profissao;
-    var altura =   this.dados_alterar_cadastro.value.altura;
-    this.pacienteProvider.alterar_paciente(
-      username_atual,
-      nome,
-      username,
-      dataNascimento,
-      sexo,
-      email,
-      celular,
-      cidade,
-      profissao,
-      tipo,
-      objetivo,
-      altura
+  private alterarCadastro(req){
+    console.log(req)
+    console.log(this.dados_paciente);
+  }
+    /*
       ).subscribe(
       data => {
         const response = (data as any);
@@ -121,9 +64,7 @@ export class AlterarCadastroPage {
           this.showAlert(response.Status, response.Mensagem);
         }
       }
-    )
-  }
-
+    )*/
 
   ionViewDidLoad() {
 
